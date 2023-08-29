50fc4927a430711afa242440e0d3530ac395f923
Maya Lee <maya.v.lee393@gmail.com>
2023-08-28 21:14:50 -0500

    add slider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import ImageSlider from "./ImageSlider"
 
 function App() {
+  const slides = [
+    {url: 'http://localhost:3000/IMG_6361.jpg',  title: 'Ham 1'},
+    {url: 'http://localhost:3000/IMG_6428.jpg',  title: 'Ham 2'},
+    {url: 'http://localhost:3000/IMG_6921.jpg',  title: 'Ham 3'},
+    {url: 'http://localhost:3000/IMG_7428.jpg',  title: 'Ham 4'},
+    {url: 'http://localhost:3000/IMG_8036.jpg',  title: 'Ham 5'}
+  ];
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Maya's Photo Gallery</h1>
+      <div style={containerStyles}>
+        <ImageSlider slides={slides}/>
+      </div>
     </div>
   );
 }
